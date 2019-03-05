84fb65be235d807164ad14bfaf343f35a1a61ed6
RadekKosiada <kosiadar@gmail.com>
2019-03-05 12:55:56 +0100

    Drawn x-circles, with hard-coded levels:

--- script.js
+++ script.js
@@ -21,29 +21,52 @@ var app = new Vue({
 
 // console.log(app._data.chartData)
 
-function drawPolarChart(options, data) {
-    console.log(options, data);
+function drawPolarChart(options, appData) {
+    console.log(options, appData);
     //creating an svg node
     var svg = d3.select("#app")
         .append("svg")
         .attr("class", "polar-chart")
         .attr("width", options.width)
         .attr("height", options.height)
-        .attr("stroke", options.fillColor)   
-        .attr("fill", options.fillColor);  
         
-    var g = d3.select(".polar-chart")
-        .append("g");   
+    var circleContainers = svg.append("g")
+        .attr("class", "circle-containers"); 
+
+
+    circleContainers.selectAll(".levels")
+    //appending data to circleContainers
+        .data(d3.range(1, options.levels + 1))
+        .enter()
+        .append("circle")
+        .attr("class", "x-circles")
+        .attr("r", function(d, i) { 
+            // console.log(i)
+            return (options.height/2) / options.levels * d; 
+        })
+        .attr("cx", options.width/2)
+        .attr("cy", options.height/2)
+        .attr("stroke", options.strokeColor)
+        .attr("stroke-dasharray", options.dashesLength)
+        .attr("stroke-width", options.dashesWidth)
+        .attr("fill", "none")
 }
+
+
 
 // storing all the options of the chart in an object;
 // for easy access, if need to change;
 // will pass it later to function that draws the chart together with data;
 //also changing the data structure wont require additional work;
 var polarChartOptions = {
-    width: "800px",
-    height: "800px",
-    fillColor: "black"
+    width: 800,
+    height: 800,
+    strokeColor: "grey",
+    //how many x-circles there will be
+    //the number should be fixed, no matter how high the bars
+    levels: 10,
+    dashesWidth: 2,
+    dashesLength: 2,
 }
 
 drawPolarChart(polarChartOptions, app._data.chartData);
