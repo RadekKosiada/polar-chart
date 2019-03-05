a96003bb277694cef8d1d7e374064a0a3220a244
RadekKosiada <kosiadar@gmail.com>
2019-03-05 10:04:30 +0100

    Added data set

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         numberOfInputs: 1,
+        'Industrial': 80,
+        // console.log(app["Core AI"])
+        'Core AI': 25,
+        'VCs': 100,
+        'CVCs': 58,
+        'Acc/includ': 45,
+        'Meetups': 12,
+        'Corp. RCs': 62,
+        'Non-corp. RCs': 80,
+        'Top unis': 60,
+        'Students': 89,
+        'AI publications': 41
+
+
     }
 })
+
+console.log(app["Core AI"])
