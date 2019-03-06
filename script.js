9bf77d2f970edf85a03f6fdca5f3f50a12e94b47
RadekKosiada <kosiadar@gmail.com>
2019-03-06 08:31:12 +0100

    Rendering first arc, but just hard-coded;

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ var app = new Vue({
     }
 })
 
-// console.log(app._data.chartData)
+console.log(app._data.chartData);
+console.log(Object.keys(app._data.chartData))
+console.log(Object.keys(app._data.chartData).length)
 
 function drawPolarChart(options, appData) {
     console.log(options, appData);
@@ -27,40 +29,61 @@ function drawPolarChart(options, appData) {
     var svg = d3.select("#app")
         .append("svg")
         .attr("class", "polar-chart")
-        .attr("width", options.width)
-        .attr("height", options.height)
-        
-    var circleContainers = svg.append("g")
-        .attr("class", "circle-containers"); 
+        .attr("width", options.size)
+        .attr("height", options.size)
 
-
-    circleContainers.selectAll(".levels")
-    //appending data to circleContainers
+    //drawing circles on svg
+    svg.selectAll(".levels")
+        //appending data to circles
         .data(d3.range(1, options.levels + 1))
         .enter()
         .append("circle")
-        .attr("class", "x-circles")
-        .attr("r", function(d, i) { 
+        .attr("class", "x-circle")
+        .attr("r", function (d, i) {
             // console.log(i)
-            return (options.height/2) / options.levels * d; 
+            return (options.size / 2) / options.levels * d;
         })
-        .attr("cx", options.width/2)
-        .attr("cy", options.height/2)
+        .attr("cx", options.size / 2)
+        .attr("cy", options.size / 2)
         .attr("stroke", options.strokeColor)
         .attr("stroke-dasharray", options.dashesLength)
         .attr("stroke-width", options.dashesWidth)
-        .attr("fill", "none")
+        .attr("fill", "none");
+
+    var numberOfBars = Object.keys(appData).length;    
+    // array with names of all dimensions
+    var nameOfBars = Object.keys(appData);
+
+    //   console.log(arc(appData));
+
+        var arc = d3.svg.arc()
+            .innerRadius(30)
+            .outerRadius(90)
+            .startAngle(0)
+            .endAngle(3) 
+
+        svg.append("path")
+            .attr("d", arc)
+            .attr("fill", "red")
+            .attr("transform", "translate(" + options.size/2 + "," + options.size/2 + ")")
+            .attr("fill-opacity", "0.75")
+
+    
 }
+
+
+
 
 
 
 // storing all the options of the chart in an object;
 // for easy access, if need to change;
 // will pass it later to function that draws the chart together with data;
-//also changing the data structure wont require additional work;
+//also changing the data structure won't require additional work;
+
 var polarChartOptions = {
-    width: 800,
-    height: 800,
+    // size will work for both width and height;
+    size: 800,
     strokeColor: "grey",
     //how many x-circles there will be
     //the number should be fixed, no matter how high the bars
